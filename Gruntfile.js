6cfbfc6ea87d6e02a0d1d562cea52e02c80a1b56
Andy Wermke <andy@dev.next-step-software.com>
2014-06-12 11:10:17 +0200

    Added jshint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,22 @@
 'use strict';
 
 module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
+    jshint: {
+      plugin: ['jquery.dim-background.js'],
+      grunt: {
+        options: {
+          node: true
+        },
+        files: {
+          src: ['Gruntfile.js']
+        }
+      }
+    },
     uglify: {
       js: {
         files: {
@@ -20,5 +32,5 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('default', 'uglify');
+  grunt.registerTask('default', ['jshint', 'uglify']);
 };
