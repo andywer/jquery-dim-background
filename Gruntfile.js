2948e0902b8c813b87d7e90d2e1e08840f362f11
Andy Wermke <andy@dev.next-step-software.com>
2014-06-12 11:38:57 +0200

    Added check-version task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
 module.exports = function (grunt) {
+
+  var fs = require('fs');
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -32,5 +35,29 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('check-version', 'Checks that the version is the same in all manifest files.', function () {
+    var jsonFromFile = function (file) {
+      return JSON.parse( fs.readFileSync(file, 'utf-8') );
+    };
+
+    var assertVersion = function (file, version) {
+      var fileVersion = jsonFromFile(file).version;
+
+      if (fileVersion !== version) {
+        errors++;
+        grunt.log.error('Version of '+file+' should be "'+version+'", but is "'+fileVersion+'"');
+      }
+    };
+
+    var errors = 0;
+    var version = jsonFromFile('dim-background.jquery.json').version;
+
+    assertVersion('bower.json', version);
+    assertVersion('package.json', version);
+
+    return errors === 0;
+  });
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'check-version']);
+
 };
